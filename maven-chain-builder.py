7f11e93984be062730151234d6283ea23ec1926e
Arie Bregman <abregman@redhat.com>
2016-01-18 16:47:09 +0200

    Minor fixes
    * less confusing logger entries
    * removed unused variables

--- maven-chain-builder.py
+++ maven-chain-builder.py
@@ -21,6 +21,7 @@ def clone_project(git_url, project, directory):
     """ Clone git repo """
     start_wd = os.getcwd()
     os.chdir(directory)
+    logger.info('Changed dir: %s', directory)
     if not os.path.exists(project):
         logger.info('Cloning: %s', project)
         git.Git().clone(git_url)
@@ -44,12 +45,15 @@ def get_subdir(url):
 def checkout(branch, project, directory):
     start_wd = os.getcwd()
     os.chdir(directory)
+    logger.info('Changed dir: %s', directory)
+    logger.info('Checking out the branch: %s', branch)
     (git.Git(project)).checkout(branch)
     os.chdir(start_wd)
 
 def apply_patch(patch_dir, project, patch_repo_name):
     start_wd = os.getcwd()
     os.chdir(project)
+    logger.info('Changed dir: %s', project)
     proj = git.Repo(project)
     logger.info('Looking for patches in %s', patch_dir)
     patches = glob(patch_dir + '/*.patch')
@@ -61,20 +65,18 @@ def apply_patch(patch_dir, project, patch_repo_name):
     os.chdir(start_wd)
 
 def clone_patch(url, project_path):
-    start_wd = os.getcwd()
     patch_url = get_git_url(url)
     patch_project_name = get_project_name(url)
     patch_branch = get_branch(url)
     subdir = get_subdir(url)
     patch_path = '/tmp/' + patch_project_name + '/' + subdir
     clone_project(patch_url, patch_project_name, '/tmp')
-    logger.info('Cloning patch: %s', patch_project_name)
+    logger.info('Checking out the branch: %s', patch_branch)
     checkout(patch_branch, patch_project_name, '/tmp')
-    logger.info('Checked out %s', patch_branch)
     apply_patch(patch_path, project_path, patch_project_name)
-    os.chdir(start_wd)
 
 def set_jvm_options(value):
+    logger.info('Setting MAVEN_OPTS to: %s', value)
     os.environ["MAVEN_OPTS"] = value
 
 def setup_logger():
@@ -94,7 +96,7 @@ def build(project, build_cmd, subdir):
     start_wd = os.getcwd()
     os.chdir(build_path)
     logger.info('The build command is: %s', build_cmd)
-    logger.info('Entered %s', build_path)
+    logger.info('Changed dir: %s', build_path)
     logger.info('Running build!')
     os.system(build_cmd + " >> /var/log/maven-chain-builder.log")
     os.chdir(start_wd)
@@ -142,8 +144,8 @@ for section in config.sections:
                 branch = get_branch(config[section][option])
                 logger.info('Branch to checkout: %s', branch)
                 git_url = get_git_url(config[section][option])
-                logger.info('Cloning: %s', git_url)
                 project_path = rand_dir + '/' + project_name
+                logger.info('Project path: %s', project_path)
                 project_top_dir = rand_dir
                 clone_project(git_url, project_name, project_top_dir)
                 checkout(branch, project_name, project_top_dir)
